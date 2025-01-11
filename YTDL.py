e0b4bd317757105018ec4eec8e6f1507d65fc7b4
Scream [dev] <167887277+scream-dev@users.noreply.github.com>
2025-01-11 15:55:55 +0300

    Update YTDL.py

--- YTDL.py
+++ YTDL.py
@@ -7,4 +7,56 @@
 # 👌 Channel @ScreamModules
 # 🧨 Blog: @ScreamDevBlog
 
-# модуль ещё делается
+import io
+import os
+from asyncio import sleep
+
+from requests import get
+from telethon import events, functions
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.types import MessageEntityTextUrl, MessageEntityUrl
+
+from .. import loader, utils
+
+@loader.tds
+class DownloaderMod(loader.Module):
+    """Downloader module"""
+
+    strings = {"name": "Downloader"}
+
+    async def dltiktokcmd(self, message):
+        """TikTok video downloader"""
+        chat = "@ttsavebot"
+        reply = await message.get_reply_message()
+        async with message.client.conversation(chat) as conv:
+            text = utils.get_args_raw(message)
+            if reply:
+                text = await message.get_reply_message()
+            await message.edit("<b>Downloading...</b>")
+            try:
+                response = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=1087584961)
+                )
+                response2 = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=1087584961)
+                )
+                response3 = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=1087584961)
+                )
+                mm = await message.client.send_message(chat, text)
+                response = await response
+                response2 = await response2
+                response3 = await response3
+                await mm.delete()
+            except YouBlockedUserError:
+                await message.edit("<code>Разблокируй @ttsavebot</code>")
+                return
+            await message.client.send_file(
+                message.to_id, response3.media, reply_to=reply
+            )
+            await message.delete()
+            await message.client(
+                functions.messages.DeleteHistoryRequest(
+                    peer="ttsavebot", max_id=0, just_clear=False, revoke=True
+                )
+            )
