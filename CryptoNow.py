928e2e3dfb8f135a9addf0d8b79070a917f42e31
Scream [dev] <167887277+scream-dev@users.noreply.github.com>
2025-01-12 21:30:21 +0300

    Create CryptoNow.py

---
+++ CryptoNow.py
@@ -0,0 +1,11 @@
+#           __..--''``---....___   _..._    __
+# /// //_.-'    .-/";  `        ``<._  ``.''_ `. / // /
+#///_.-' _..--.'_    \                    `( ) ) // //
+#/ (_..-' // (< _     ;_..__               ; `' / ///
+# / // // //  `-._,_)' // / ``--...____..-' /// / //
+# 🐈 Module writed @ScreamDev 
+# 👌 Channel @ScreamModules
+# 🧨 Blog: @ScreamDevBlog
+# meta developer: @ScreamDev, @yg_modules
+
+# in dev
