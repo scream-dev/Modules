e68e38a0852996d82821e3d73a1bd2b0b7af2cae
Scream [dev] <167887277+scream-dev@users.noreply.github.com>
2025-01-23 20:13:49 +0300

    Create CulturalChat.py

---
+++ archive/CulturalChat.py
@@ -0,0 +1,19 @@
+#           __..--''``---....___   _..._    __
+# /// //_.-'    .-/";  `        ``<._  ``.''_ `. / // /
+#///_.-' _..--.'_    \                    `( ) ) // //
+#/ (_..-' // (< _     ;_..__               ; `' / ///
+# / // // //  `-._,_)' // / ``--...____..-' /// / //
+# 🐈 Module writed @ScreamDev, AuroraModules 
+# 👌 Channel @ScreamModules
+# 🧨 Blog: @ScreamDevBlog
+
+# Module is modificarion BanWord by AuroraModules
+# Original: https://raw.githubusercontent.com/amm1edev/ame_repo/main/BanWord.py
+# * 🔒 Code is licensed under GNU AGPLv3
+# * 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+# * ⛔️ You CANNOT edit this file without direct permission from the author.
+# * ⛔️ You CANNOT distribute this file if you have modified it without the direct permission of the author.
+
+# meta developer: @ScreamDev, AuroraModules
+# meta banner: 
+# meta pic: 
